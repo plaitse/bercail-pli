f92e1d17684929b4e98c8de7020c036f92e4a811
plaitse <riviera_paradise@hotmail.fr>
2017-03-19 21:12:58 +0100

    Filters 1.0 - Result page

--- resources/views/pages/filter-result-page.blade.php
+++ resources/views/pages/filter-result-page.blade.php
@@ -1,65 +1,120 @@
 <div class="filter-box-ad-result project-box-ad-result">
 	<p class="filter-title-box-ad-result">Quel est votre projet ?</p>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Acheter</a></li>
-	    <li><a href="#">Louer</a></li>
-	    <li><a href="#">Construire sa maison</a></li>
-	    <li><a href="#">Investir</a></li>
-	    <li><a href="#">Viager</a></li>
-  	</ul>
+	<form action="">
+		<ul class="nav navbar-nav filter-change-box-ad-result">
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="project" value="buy">Acheter</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="project" value="rent" checked>Louer</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="project" value="build">Construire sa maison</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="project" value="invest">Investir</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="project" value="annuity">Viager</a></li>
+	  	</ul>
+	  	<div class="separation-result-box-ad-result"></div>
+	  	<input class="btn filter-change-box-ad-result result-filter-ad-result" 
+			type="submit" name="surface" value="Voir les 448 annonces">
+	</form>
 </div>
 <div class="filter-box-ad-result type-box-ad-result">
-	<p class="filter-title-box-ad-result">Quel type de bien cherchez-vous ?</p>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#" class="round-filter-ad-result">Habitation</a></li>
-	    <li><a href="#" class="round-filter-ad-result">Bureaux et commerces</a></li>
-	    <li><a href="#" class="round-filter-ad-result">Autres</a></li>
-  	</ul>
-	<div class="separation-change-box-ad-result"></div>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Appartement</a></li>
-	    <li><a href="#">Maison et Villa</a></li>
-	    <li><a href="#">Loft / Atelier / Surface</a></li>
-	    <li><a href="#">Hôtel particulier</a></li>
-	    <li><a href="#">Château</a></li>
-  	</ul>
-  	<div class="separation-result-box-ad-result"></div>
-  	<a href="#" class="filter-change-box-ad-result result-filter-ad-result">Voir les 448 annonces</a>
+	<form action="">
+		<p class="filter-title-box-ad-result">Quel type de bien cherchez-vous ?</p>
+		<ul class="nav navbar-nav filter-change-box-ad-result">
+		    <li><a href="#" class="round-filter-ad-result">
+		    	<input class="radiobtn-ad-result" type="radio" name="type" value="residence" checked>Habitation</a></li>
+		    <li><a href="#" class="round-filter-ad-result">
+		    	<input class="radiobtn-ad-result" type="radio" name="type" value="office">Bureaux et commerces</a></li>
+		    <li><a href="#" class="round-filter-ad-result">
+		    	<input class="radiobtn-ad-result" type="radio" name="type" value="other">Autres</a></li>
+	  	</ul>
+		<div class="separation-change-box-ad-result"></div>
+		<ul class="nav navbar-nav filter-change-box-ad-result">
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="type-detail" value="apartment" checked>Appartement</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="type-detail" value="house">Maison et Villa</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="type-detail" value="loft">Loft / Atelier / Surface</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="type-detail" value="hotel">Hôtel particulier</a></li>
+		    <li><a href="#">
+		    	<input class="radiobtn-ad-result" type="radio" name="type-detail" value="castle">Château</a></li>
+	  	</ul>
+	  	<div class="separation-result-box-ad-result"></div>
+	  	<input class="btn filter-change-box-ad-result result-filter-ad-result" 
+			type="submit" name="surface" value="Voir les 448 annonces">
+	</form>
 </div>
 <div class="filter-box-ad-result rooms-box-ad-result">
 	<p class="filter-title-box-ad-result">De combien de pièces ?</p>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Studio</a></li>
-	    <li><a href="#">2</a></li>
-	    <li><a href="#">3</a></li>
-	    <li><a href="#">4</a></li>
-	    <li><a href="#">5 et plus</a></li>
-  	</ul>
+	<form action="">
+		<div class="row">
+			<div class="col-md-6">
+				<p class="rooms-criteria-ad-result">Nombre de pièces</p>
+				<ul class="nav navbar-nav filter-change-box-ad-result">
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="part" value="1part" checked>Studio</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="part" value="2part">2</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="part" value="3part">3</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="part" value="4part">4</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="part" value="5part">5 et plus</a></li>
+			  	</ul>
+			</div>
+			<div class="col-md-6">
+				<p class="rooms-criteria-ad-result">Nombre de chambres</p>
+				<ul class="nav navbar-nav filter-change-box-ad-result">
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="room" value="1room" checked>1</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="room" value="2room">2</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="room" value="3room">3</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="room" value="4room">4</a></li>
+				    <li><a href="#">
+				    	<input class="radiobtn-ad-result" type="radio" name="room" value="5room">5 et plus</a></li>
+			  	</ul>
+			</div>
+		 </div>
+	 	<input class="btn filter-change-box-ad-result result-filter-ad-result" 
+			type="submit" name="surface" value="Voir les 448 annonces">
+	</form>
 </div>
 <div class="filter-box-ad-result surface-box-ad-result">
 	<p class="filter-title-box-ad-result">De quelle surface ?</p>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Surface min</a></li>
-	    <li><a href="#">Surface max</a></li>
-  	</ul>
+	<form action="">
+		De <input class"text-input-ad-result" type="text" name="surface-min" placeholder="Surface min"> m2
+		juqu'à <input class"text-input-ad-result" type="text" name="surface-max" placeholder="Surface max"> m2
+		<div class="separation-result-box-ad-result"></div>
+		<input class="btn filter-change-box-ad-result result-filter-ad-result" 
+			type="submit" name="surface" value="Voir les 448 annonces">
+	</form>
 </div>
 <div class="filter-box-ad-result place-box-ad-result">
 	<p class="filter-title-box-ad-result">Où souhaitez-vous vivre ?</p>
 	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Un autre lieu ?</a></li>
+	    <li>À venir...</li>
   	</ul>
 </div>
 <div class="filter-box-ad-result budget-box-ad-result">
 	<p class="filter-title-box-ad-result">Quel est votre budget ?</p>
-	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Budget min</a></li>
-	    <li><a href="#">Budget max</a></li>
-  	</ul>
+	<form action="">
+		De <input class"text-input-ad-result" type="text" name="budget-min" placeholder="Budget min"> €
+		juqu'à <input class"text-input-ad-result" type="text" name="budget-max" value="1500" placeholder="Budget max"> €
+		<div class="separation-result-box-ad-result"></div>
+		<input class="btn filter-change-box-ad-result result-filter-ad-result" 
+			type="submit" name="budget" value="Voir les 448 annonces">
+	</form>
 </div>
 <div class="filter-box-ad-result more-box-ad-result">
 	<p class="filter-title-box-ad-result">Surface du terrain</p>
 	<ul class="nav navbar-nav filter-change-box-ad-result">
-	    <li><a href="#">Surface min</a></li>
-	    <li><a href="#">Surface max</a></li>
+	    <li>À venir...</li>
   	</ul>
 </div>
